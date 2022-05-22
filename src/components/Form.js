3645c4f07922cb8cf84148106300bcd07ace917c
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 19:42:29 -0400

    Generate api query specifications for each prompt inside an onSubmit function

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,12 +12,27 @@ const Form = () => {
     `Generate a mission statement for a ${query} business`,
   ];
 
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    prompts.forEach((prompt) => {
+      const query = {
+        prompt: prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1.0,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      };
+    });
+  }
+
   return (
     <StyledForm className="tagline-mission-statement-form">
       <h1 className="section-header">
         Describe your business in one word and let us do the rest.
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           name="query"
