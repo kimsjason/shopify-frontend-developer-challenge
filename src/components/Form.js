88b3e69a1403e0dc72841c3fea783c6ec2456030
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 19:25:55 -0400

    Add form html elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
-const StyledForm = styled.div`
-  height: 200vh;
-`;
+const StyledForm = styled.div``;
 
 const Form = () => {
   return (
-    <StyledForm className="tagline-mission-statement-form">Form</StyledForm>
+    <StyledForm className="tagline-mission-statement-form">
+      <h1 className="section-header">
+        Describe your business in one word and let us do the rest.
+      </h1>
+      <form>
+        <input type="text" placeholder="Enter query" />
+        <button type="submit" className="submit-button">
+          Generate Content
+        </button>
+      </form>
+    </StyledForm>
   );
 };
 
