ab1947def32ed1ed09cd5b8d3e781f6bece3c37e
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 20:23:47 -0400

    Extract fetch call to separate function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const StyledForm = styled.div``;
@@ -12,19 +12,44 @@ const Form = () => {
     `Generate a mission statement for a ${query} business`,
   ];
 
+  const fetchAPI = async (prompt) => {
+    const query = {
+      prompt: prompt,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1.0,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+
+    const response = await fetch(
+      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify(query),
+      }
+    );
+
+    const data = await response.json();
+
+    return data.choices[0].text;
+  };
+
   async function onSubmit(event) {
     event.preventDefault();
 
-    prompts.forEach((prompt) => {
-      const query = {
-        prompt: prompt,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1.0,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      };
-    });
+    const completion = {
+      query: query,
+      tagline: await fetchAPI(prompts[0]),
+      mission_statement: await fetchAPI(prompts[1]),
+    };
+
+    setQuery("");
+    setResult(completion);
   }
 
   return (
@@ -38,7 +63,7 @@ const Form = () => {
           name="query"
           placeholder="Enter query"
           className="query-input"
-          onChange={(e) => e.target.value}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className="submit-button">
           Generate Content
