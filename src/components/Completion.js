b822703ca5b6907827e1ed6e69028ca7cdcd232b
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 21:34:18 -0400

    Create completion component

---
+++ src/components/Completion.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledCompletion = styled.div``;
+
+const Completion = () => {
+  return <StyledCompletion className="completion"></StyledCompletion>;
+};
+
+export default Completion;
