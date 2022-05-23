8d737a4ede1687799cb049f633d0eb399c92cd81
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 21:20:33 -0400

    Add test to verify multiple completions stored in array

--- src/tests/Form.test.js
+++ src/tests/Form.test.js
@@ -24,3 +24,27 @@ it("retrieves an openai response when the fetch api is called", async () => {
 
   expect(data.choices[0].text !== undefined).toBe(true);
 });
+
+it("appends new responses to the user's previous queries / completions", () => {
+  let completions = [];
+
+  const fetchAPI = async () => {
+    return "a text response";
+  };
+
+  const onSubmit = async () => {
+    const completion = {
+      query: "test",
+      tagline: fetchAPI(),
+      mission_statement: fetchAPI(),
+      timestamp: new Date(Date.now()),
+    };
+
+    completions = [...completions, completion];
+  };
+
+  onSubmit();
+  onSubmit();
+
+  expect(completions.length).toBe(2);
+});
