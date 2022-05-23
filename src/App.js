96c8e796ad59e7747e5b20e112ec14a196286fe3
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 22:14:42 -0400

    Persist completions with localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Completions from "./components/Completions";
 import Form from "./components/Form";
@@ -6,7 +6,17 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 function App() {
-  const [completions, setCompletions] = useState([]);
+  const [completions, setCompletions] = useState(() => {
+    // get locally stored completions if available
+    const saved = localStorage.getItem("completions");
+    const savedCompletions = JSON.parse(saved);
+    return savedCompletions || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("completions", JSON.stringify(completions));
+    console.log("adding to local storage...");
+  }, [completions]);
 
   return (
     <div className="App">
