d5e02d49baf2732530daafcf723e694dac6512ec
Jason Kim <jason.s.kim95@gmail.com>
2022-05-22 22:14:26 -0400

    Sort completions from newest to oldest responses

--- src/components/Completions.js
+++ src/components/Completions.js
@@ -3,12 +3,19 @@ import Completion from "./Completion";
 
 const StyledCompletions = styled.div`
   padding: 64px 5%;
+
+  @media only screen and (min-width: 769px) {
+    padding: 64px 15%;
+  }
 `;
 
 const Completions = ({ completions }) => {
+  // sorts completions array from newest to oldest responses
+  const sortedCompletions = completions.slice(0).reverse();
+
   return (
     <StyledCompletions className="completions">
-      {completions.map((completion) => {
+      {sortedCompletions.map((completion) => {
         return <Completion completion={completion} />;
       })}
     </StyledCompletions>
